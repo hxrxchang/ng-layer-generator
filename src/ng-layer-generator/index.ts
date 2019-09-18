9303f539d34f1b0bd55113b65e589ff8a966f527
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-09-18 13:24:56 +0900

    generate file in src/app/

--- src/ng-layer-generator/index.ts
+++ src/ng-layer-generator/index.ts
@@ -5,18 +5,21 @@ import {
   apply,
   mergeWith,
   template,
-  url
+  url,
+  move
 } from '@angular-devkit/schematics';
 
 export function ngLayerGenerator(_options: any): Rule {
   return (_, _context: SchematicContext) => {
+    const path = '/src/app';
     return mergeWith(
       apply(url('./files'), [
         template({
           ...strings,
           name: _options.name,
           type: _options.type
-        })
+        }),
+        move(path)
       ])
     );
   };
