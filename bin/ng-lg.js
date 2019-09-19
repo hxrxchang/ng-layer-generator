c51fa1a534a37c6a88b2f73f89372c4ed000d43d
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-09-20 01:16:33 +0900

    change --color option position

--- bin/ng-lg.js
+++ bin/ng-lg.js
@@ -2,14 +2,14 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const arguments = process.argv.slice(2);
 
-let command = 'yarn schematics ng-layer-generator:ng-layer-generator --color';
+let command = 'yarn schematics ng-layer-generator:ng-layer-generator';
 arguments.forEach(arg => {
   command = `${command} ${arg}`;
 });
 
 async function main() {
   try {
-    const { stdout } = await exec(command);
+    const { stdout } = await exec(`${command} --color`);
     console.log(stdout);
   } catch (e) {
     console.error(e);
