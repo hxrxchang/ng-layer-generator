9d96bf10392572809048ab813d358362090333eb
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-12-08 17:38:22 +0900

    add test

--- src/ng-layer-generator/index_spec.ts
+++ src/ng-layer-generator/index_spec.ts
@@ -2,15 +2,66 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-
 const collectionPath = path.join(__dirname, '../collection.json');
 
-
 describe('ng-layer-generator', () => {
-  it('works', () => {
+  it('specify name and type', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('ng-layer-generator', {}, Tree.empty());
+    const options = {
+      name: 'sample',
+      type: 'usecase'
+    };
+    const tree = runner.runSchematic(
+      'ng-layer-generator',
+      options,
+      Tree.empty()
+    );
+    const expectedOutput = [
+      '/src/app/sample.usecase.spec.ts',
+      '/src/app/sample.usecase.ts'
+    ];
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toEqual(expectedOutput);
+  });
+
+  it('specify name, type, and path', () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const options = {
+      name: 'sample',
+      type: 'usecase',
+      path: 'usecases'
+    };
+    const tree = runner.runSchematic(
+      'ng-layer-generator',
+      options,
+      Tree.empty()
+    );
+    const expectedOutput = [
+      '/src/app/usecases/sample.usecase.spec.ts',
+      '/src/app/usecases/sample.usecase.ts'
+    ];
+
+    expect(tree.files).toEqual(expectedOutput);
+  });
+
+  it('specify name, type, path, and  project', () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const options = {
+      name: 'sample',
+      type: 'usecase',
+      path: 'usecases',
+      project: 'app1'
+    };
+    const tree = runner.runSchematic(
+      'ng-layer-generator',
+      options,
+      Tree.empty()
+    );
+    const expectedOutput = [
+      '/projects/app1/src/app/usecases/sample.usecase.spec.ts',
+      '/projects/app1/src/app/usecases/sample.usecase.ts'
+    ];
+
+    expect(tree.files).toEqual(expectedOutput);
   });
 });
